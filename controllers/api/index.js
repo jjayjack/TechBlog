8410a8dd9d169ffc72e018f86b2b1968e600b5a6
jjayjack <jocelynjayjack@gmail.com>
2021-05-12 23:16:21 -0500

    Updated routes to include homeRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes.js');
+const homeRoutes = require('./homeRoutes.js');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/', homeRoutes);
+
 
 module.exports = router;

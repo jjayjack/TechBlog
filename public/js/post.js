cb2613cd4aeb6598eecf1381d29dd5f92703d723
jjayjack <jocelynjayjack@gmail.com>
2021-05-13 22:08:53 -0500

    Post js for client side

---
+++ public/js/post.js
@@ -0,0 +1,20 @@
+const postFormHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#title-content').value.trim();
+    const description = document.querySelector('#content').value.trim();
+
+    const response = await fetch('/posts', {
+        method: 'POST',
+        body: JSON.stringify({ title, description }),
+        headers: {'Content-Type': 'application/json'}
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Unable to post')
+    }
+}
+
+document.querySelector('.post-form').addEventListener('submit', postFormHandler);

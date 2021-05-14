9d9a390433588af2b4b633f426408d374c774d10
jjayjack <jocelynjayjack@gmail.com>
2021-05-13 22:07:50 -0500

    Updated document selectors

--- public/js/login.js
+++ public/js/login.js
@@ -3,12 +3,12 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
   
     // Gather the data from the form elements on the page
-    const email = document.querySelector('#email-login').value.trim();
+    const email = document.querySelector('#user-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
     if (email && password) {
       // Send the e-mail and password to the server
-      const response = await fetch('/api/users/login', {
+      const response = await fetch('/users/login', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },

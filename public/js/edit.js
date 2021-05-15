3b46ab4ddc269b4e4b200bfd799a387879999aff
jjayjack <jocelynjayjack@gmail.com>
2021-05-15 14:50:05 -0500

    editing post js on click

---
+++ public/js/edit.js
@@ -0,0 +1,19 @@
+const editPost = async(event) =>{
+    event.preventDefault();
+    const id = event.target.dataset.id
+    const prevTitle = document.querySelector('#title-content').value.trim();
+    const prevContent = document.querySelector('#content').value.trim();
+
+    const edit = await fetch('/posts/'+ id, {
+        method: 'PUT',
+        body: JSON.stringify({ prevTitle, prevContent }),
+        headers: { 'Content-Type': 'application/json'}
+    });
+    if (edit.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert('Unable to edit')
+    }
+}
+
+document.querySelector('.post-form').addEventListener('submit', editPost);

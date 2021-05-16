a086710f48cf137733eb2a302992582adafeda99
jjayjack <jocelynjayjack@gmail.com>
2021-05-15 23:51:48 -0500

    Updated edit js to reflect correct naming

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,12 +1,13 @@
+
 const editPost = async(event) =>{
     event.preventDefault();
     const id = event.target.dataset.id
-    const prevTitle = document.querySelector('#title-content').value.trim();
-    const prevContent = document.querySelector('#content').value.trim();
+    const title = document.querySelector('#title-content').value.trim();
+    const description = document.querySelector('#content').value.trim();
 
     const edit = await fetch('/posts/'+ id, {
         method: 'PUT',
-        body: JSON.stringify({ prevTitle, prevContent }),
+        body: JSON.stringify({ title, description }),
         headers: { 'Content-Type': 'application/json'}
     });
     if (edit.ok) {
